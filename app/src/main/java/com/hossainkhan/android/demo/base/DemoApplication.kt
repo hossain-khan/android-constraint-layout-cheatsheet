c909ff3cb14bc91badc866d1c44d5bbec457f642
Hossain Khan <amardeshbd@gmail.com>
2018-04-30 23:24:20 -0400

    [ADDED] [#6] Dagger dependency injection support. Added base components and modules for `MainActivity` as POC.

--- app/src/main/java/com/hossainkhan/android/demo/base/DemoApplication.kt
+++ app/src/main/java/com/hossainkhan/android/demo/base/DemoApplication.kt
@@ -1,12 +1,35 @@
+/*
+ * Copyright (c) 2018 Hossain Khan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hossainkhan.android.demo.base
 
+import android.app.Activity
 import android.app.Application
 import com.hossainkhan.android.demo.BuildConfig
+import com.hossainkhan.android.demo.dagger.DaggerDemoApplicationComponent
 import com.squareup.leakcanary.LeakCanary
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
 import timber.log.Timber
+import javax.inject.Inject
 
 
-class DemoApplication : Application() {
+class DemoApplication : Application(), HasActivityInjector {
+    @Inject lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
 
     override fun onCreate() {
         super.onCreate()
@@ -17,8 +40,16 @@ class DemoApplication : Application() {
         }
         LeakCanary.install(this)
 
+        // Prepares dagger
+        DaggerDemoApplicationComponent.create().inject(this)
+
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
     }
+
+    override fun activityInjector(): AndroidInjector<Activity> {
+        return dispatchingAndroidInjector
+    }
+
 }
